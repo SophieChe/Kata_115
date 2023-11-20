71d6362f1f25775f6e52fe182da5fac25f9e32a1
Admin <sofiya.chebotareva@bk.ru>
2023-11-20 18:16:39 +0100

    as of 20.11

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,4 +1,11 @@
 package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,7 +18,7 @@ public class Util {
     private static final String BD_USERNAME = "root";
     private static final String BD_PASSWORD = "root";
     private static final String BD_URL = "jdbc:mysql://localhost:3306/katabd";
-
+    private static SessionFactory sessionFactory;
 
     public Connection getConnection() {
         try {
@@ -29,4 +36,19 @@ public class Util {
         return util;
     }
 
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new org.hibernate.cfg.Configuration()
+                        .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/katabd")
+                        .setProperty("hibernate.connection.username", "root")
+                        .setProperty("hibernate.connection.password", "root")
+                        .setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+                sessionFactory = configuration.addAnnotatedClass(User.class).buildSessionFactory();
+            } catch (HibernateException e) {
+                System.out.println("Исключение" + e);
+            }
+        }
+        return sessionFactory;
+    }
 }
